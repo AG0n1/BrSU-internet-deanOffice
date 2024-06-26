b8ae38d2e2f047995880c5b973aa3112f3b05024
Matthew Markovets <mmarkovec15072003@gmail.com>
2024-06-26 16:08:52 +0300

    did task2

--- app/src/components/tasks/Task2.jsx
+++ app/src/components/tasks/Task2.jsx
@@ -1,5 +1,40 @@
 function Task2() {
+    let values = [],
+        result = 1,
+        step = 0
 
+    const countProduct = () => {
+        let txt = document.getElementById("task1_textarea"),
+            paste = document.getElementById("task1_paste");
+        values = txt.value.split("\n");
+
+        for (let i = 0; i < values.length; i++) {
+            if (!isNaN(parseInt(values[i]))) {
+                result *= parseInt(values[i]);
+            } else {
+                continue;
+            }
+        }
+
+        paste.innerHTML = result;
+        result = 1
+    }
+
+    return (
+        <div className="lab2">
+            <h1>Задание 1</h1>
+            <div className="practice">
+                <textarea onChange={countProduct} id="task1_textarea"></textarea>
+                <p id="task1_paste">{result}</p>
+            </div>
+
+            <h1>Задание 2</h1>
+            <div className="practice">
+                <textarea onChange={countProduct} id="task1_textarea"></textarea>
+                <p id="task1_paste">{result}</p>
+            </div>
+        </div>
+    );
 }
 
-export default Task2
+export default Task2;
