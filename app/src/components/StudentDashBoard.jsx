61e9630de45cd9fc847ea523a24bf85c9146dc10
Matthew Markovets <mmarkovec15072003@gmail.com>
2024-07-09 13:15:44 +0300

    fixes

--- app/src/components/StudentDashBoard.jsx
+++ app/src/components/StudentDashBoard.jsx
@@ -41,29 +41,57 @@ const StudentDashboard = () => {
 
           <div className="days">
             <div className="day" id="monday">
-                <h5>
+                <h3>
                   Понедельник
-                </h5>
+                </h3>
+                <div>
+                  
+                </div>
             </div>
 
             <div className="day" id="tuesday">
-
+                <h3>
+                  Вторник
+                </h3>
+                <div>
+                  
+                </div>
             </div>
 
             <div className="day" id="wednesday">
-
+                <h3>
+                  Среда
+                </h3>
+                <div>
+                  
+                </div>
             </div>
 
             <div className="day" id="thursday">
-
+                <h3>
+                  Четверг
+                </h3>
+                <div>
+                  
+                </div>
             </div>
 
             <div className="day" id="friday">
-
-            </div>ы
+                <h3>
+                  Пятница
+                </h3>
+                <div>
+                  
+                </div>
+            </div>
 
             <div className="day" id="saturday">
-
+                <h3>
+                  Суббота
+                </h3>
+                <div>
+                  
+                </div>
             </div>
           </div>
 
